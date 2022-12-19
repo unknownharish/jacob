6e1ca576c7f8fd5d9e930be538fb07bc33818a99
harish <chandharish198@gmail.com>
2022-12-19 21:50:30 +0530

    section 2 complete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,14 +11,14 @@ export default function MainHomepage() {
     return (
         <div className='h-screen overflow-y-hidden'>
 
-            <div className=' absolute top-12 w-full flex justify-center px-4 xl:hidden'>
-                <Image  src="/images/startup/splashLogo.png" height={150} width={500} />
+            <div className=' absolute top-12 w-full flex justify-center px-4 '>
+                <Image  src="/images/startup/splashLogo.png" height={130} width={500} />
             </div>
-            <div className=' absolute top-10 left-5 w-full hidden justify-center px-4 xl:flex'>
+            {/* <div className=' absolute top-10 left-5 w-full hidden justify-center px-4 xl:flex'>
                 <Image  src="/images/startup/splashLogo.png" height={50} width={200} />
-            </div>
+            </div> */}
             {/* object-cover */}
-            <img src="images/startup/main.png" className='h-screen w-screen  object-cover lg:object-bottom ' alt="" />
+            <img src="images/startup/main.png" className='h-screen w-screen  object-cover  ' alt="" />
 
             <div className='h-[10vh] flex flex-col justify-center absolute bottom-10 w-full'>
 
