e83a036cbdf887509e6b3d0b3001a67e34ea227b
harish <chandharish198@gmail.com>
2022-12-19 20:12:31 +0530

    yt images solved

--- src/Components/Main/kids/index.tsx
+++ src/Components/Main/kids/index.tsx
@@ -5,7 +5,7 @@ import CardComponent from "./card";
 
 export function MainComponent() {
 
-  const ytlinks = [{ link: 'https://youtu.be/gTWKGpDi57c', img: '/images/Kids/yt1.jpg', text: 'Barquito chiquitico' }, { link: 'https://youtu.be/5vG5Bo2XK-c', img: '/images/Kids/yt2.jpg', text: 'Si la tierra tierra fuera' }]
+  const ytlinks = [{ link: 'https://youtu.be/gTWKGpDi57c', img: '/images/Kids/yt1.webp', text: 'Barquito chiquitico' }, { link: 'https://youtu.be/5vG5Bo2XK-c', img: '/images/Kids/yt2.jpg', text: 'Si la tierra tierra fuera' }]
 
   return (
     <main className="flex-1 flex flex-col justify-center items-center">
@@ -47,7 +47,7 @@ export function MainComponent() {
               <img
                 src={'/images/action/GAMES.webp'}
                 alt="Games"
-                className="h-[250px] w-[150px] md:h-[350px] md:w-[200px]"
+                className="h-[250px] w-[150px] md:h-[350px] md:w-[200px] "
               />
             </div>
 
@@ -58,15 +58,15 @@ export function MainComponent() {
 
         </div>
 
-        <div className="flex justify-center w-10/12 sm:w-5/12 lg:w-4/12 md:justify-between flex-wrap   mt-5 ">
+        <div className="flex justify-center w-8/12 sm:w-10/12  md:w-10/12 md:justify-around lg:justify-center flex-wrap   mt-5 ">
 
           {ytlinks.map((x, idx) => (
 
-            <div key={idx}>
+            <div key={idx} className='sm:w-6/12 md:w-6/12  lg:w-4/12'>
               <a href={x.link} target={'_blank'} className='m-4 px-2 flex flex-col'>
 
                 <div className="flex justify-center items-center px-2 py-2   bg-purple-pattern rounded-lg relative cursor-pointer">
-                  <img src={x.img} alt="" className=""
+                  <img src={x.img} alt="" className="hover:brightness-110"
                   />
 
                 </div>
