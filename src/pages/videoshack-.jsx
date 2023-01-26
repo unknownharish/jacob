198c8b4ada233378f591d39fe2e6b74009ef66aa
harish <chandharish198@gmaiul.com>
2023-01-26 19:12:15 +0530

    revert

--- src/pages/videoshack-.jsx
+++ src/pages/videoshack-.jsx
@@ -14,7 +14,6 @@ export default function YtMain({ data }) {
     ])
 
 
-    const tiktok = [{ link: 'ZMYRfm2sD' }, { link: 'ZMYRfbRWN' }]
 
     useEffect(() => {
 
@@ -28,20 +27,10 @@ export default function YtMain({ data }) {
 
     let store = useSelector((x) => x.store);
 
-    if (ytlinks.length == 0) {
-        return <div className='flex justify-center items-center h-screen bg-gray-50'>
-            <Image src={'/images/yt/loader.gif'}
-                height={'400'}
-                width={'500'}
-                className=' bg-gray-400'
-            />
-        </div>
-    }
-
+  
     return (
         <>
 
-            <Script strategy='lazyOnload' src="https://www.tiktok.com/embed.js"></Script>
 
             <HeaderComponent image={'/images/dugutopia/Header.png'} />
             <div className='  bg-[#f4f481] w-[100vw] min-h-screen '>
@@ -55,7 +44,7 @@ export default function YtMain({ data }) {
                     />
                 </div>
 
-                <p className='text-center my-2'>Featured Videos</p>
+                <p className='text-center my-2'>  </p>
                 <div className='flex flex-wrap justify-around xl:justify-center items-center  md:w-full lg:w-11/12 xl:w-7/12 m-auto pb-40 '>
                     {ytlinks.map(x => {
 
@@ -70,19 +59,7 @@ export default function YtMain({ data }) {
 
 
 
-                <p className='text-center my-2'>Tiktok Videos</p>
-                <div className='flex flex-wrap justify-around xl:justify-center items-center  md:w-full lg:w-11/12 xl:w-7/12 m-auto pb-40 '>
-                    {
-                        tiktok.map(x => {
-
-                            return <div className='flex flex-col p-5 md:w-5/12 m-5'>
-                                <iframe width="370" height="470" src={`https://www.tiktok.com/wedugut/video/${x.link}`} title="Tiktok video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
-                                <p className='my-3 w-full md:w-10/12'>{x.text}</p>
-                            </div>
-                        })
-                    }
-
-                </div>
+               
             </div>
             <div className='bg-[#4617ab] h-10 w-full flex justify-center items-center'>
                 <p className='text-white'>DuGut 2022, ALL right reserved &#169;</p>
