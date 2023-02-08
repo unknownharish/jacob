f32551271a098d60aef7dfa951dcc39e8556fa3f
harish <chandharish198@gmaiul.com>
2023-02-08 22:11:22 +0530

    sidenavbar updated

--- src/Components/sidebar/Sidebar.tsx
+++ src/Components/sidebar/Sidebar.tsx
@@ -42,7 +42,7 @@ export default function Sidebar({ open, setopen }: any) {
 
             <div onClick={() => setopen(false)} className='bg-gray-700 h-screen opacity-25 absolute z-10 lg:w-4/6   '></div>
 
-            <div className='bg-black px-2 text-white w-1/2 pt-10 fixed lg:w-2/6 md:px-16 md:pt-6 overflow-x-hidden absloute right-0'  >
+            <div className='bg-black px-2  text-white w-1/2 pt-10 fixed lg:w-2/6 md:px-16 md:pt-6 overflow-x-hidden absloute right-0 min-h-screen'  >
 
 
                 {headerLinks.map((x, idx) => {
@@ -51,9 +51,9 @@ export default function Sidebar({ open, setopen }: any) {
 
                     if (x.a) {
                         return (
-                            <a href={x.link} target={'blank'}>
+                            <a key={idx} href={x.link} target={'blank'}>
                                 <div className='flex my-2 items-center cursor-pointer '>
-                                <p className='  my-3 font-bold ml-[36px]  lg:ml-[5px] xl:ml-[108px] text-md md:text-lg'>{x.text}</p>
+                                    <p className='  my-3 font-bold ml-[36px]  lg:ml-[5px] xl:ml-[108px] text-md md:text-lg'>{x.text}</p>
                                 </div>
                             </a>
                         )
@@ -147,7 +147,7 @@ export default function Sidebar({ open, setopen }: any) {
 
 
 
-{/* 
+                {/* 
                 <div className='flex flex-col text-xs md:text-sm  justify-center w-3/4 text-md mx-4 mt-8 md:my-2  md:mt-4  '>
 
                     <div className='flex justify-around items-center w-[100%] '>
@@ -187,73 +187,77 @@ export default function Sidebar({ open, setopen }: any) {
 
                 </div> */}
 
-                <p className='text-center font-light mt-6 mb-2  md:w-3/4 md:mt-2 md:ml-8 '>Connect With us:</p>
+                <div className='absolute bottom-0 w-4/6'>
 
-                {/* sm links */}
-                <div className='flex justify-around md:w-3/4 sm:w-5/6 m-auto  md:ml-8 '>
-                    <div className='mx-1'>
-                        <a href="https://www.facebook.com/wedugut/" target={'blank'}>
 
-                            <Image src={'/images/common/facebook.svg'} height={15} width={15} />
-                        </a>
+
+                    <p className='text-center font-light mt-6 mb-2  md:w-3/4 md:mt-2 md:ml-8 '>Connect With us:</p>
+
+                    {/* sm links */}
+                    <div className='flex justify-around md:w-3/4 sm:w-5/6 m-auto  md:ml-8 '>
+                        <div className='mx-1'>
+                            <a href="https://www.facebook.com/wedugut/" target={'blank'}>
+
+                                <Image src={'/images/common/facebook.svg'} height={15} width={15} />
+                            </a>
+                        </div>
+                        <div className='mx-1'>
+                            <a href="https://www.instagram.com/wedugut/" target={'blank'}>
+
+                                <Image src={'/images/common/instagram.svg'} height={15} width={15} />
+                            </a>
+                        </div>
+                        <div className='mx-1'>
+                            <a href="https://twitter.com/wedugut" target={'blank'}>
+
+                                <Image src={'/images/common/twitter.svg'} height={15} width={15} />
+                            </a>
+                        </div>
+                        <div className='mx-1'>
+                            <a href="https://www.tiktok.com/@wedugut" target={'blank'}>
+
+                                <Image src={'/images/common/tiktok.svg'} height={15} width={15} />
+                            </a>
+                        </div>
+                        <div className='mx-1'>
+                            <a href="https://www.youtube.com/channel/UCyg87GirrddSvia8TqeWepA" target={'blank'}>
+
+                                <Image src={'/images/common/youtube.svg'} height={15} width={15} />
+                            </a>
+                        </div>
+                        <div className='mx-1'>
+                            <a href="https://pt.linkedin.com/company/dugutapp" target={'blank'}>
+
+                                <Image src={'/images/common/linkedin.svg'} height={15} width={15} />
+                            </a>
+                        </div>
                     </div>
-                    <div className='mx-1'>
-                        <a href="https://www.instagram.com/wedugut/" target={'blank'}>
 
-                            <Image src={'/images/common/instagram.svg'} height={15} width={15} />
-                        </a>
-                    </div>
-                    <div className='mx-1'>
-                        <a href="https://twitter.com/wedugut" target={'blank'}>
+                    <div className=' flex mt-2 mb-1 font-light justify-center text-xs md:flex md:justify-center md:w-3/4  md:ml-8'>
 
-                            <Image src={'/images/common/twitter.svg'} height={15} width={15} />
+                        <a href="https://dugut.app/privacy/DuGut-Privacy-Policy.pdf" target={'blank'}>
+                            <p className='mr-2 cursor-pointer'>Privacy</p>
                         </a>
-                    </div>
-                    <div className='mx-1'>
-                        <a href="https://www.tiktok.com/@wedugut" target={'blank'}>
 
-                            <Image src={'/images/common/tiktok.svg'} height={15} width={15} />
-                        </a>
-                    </div>
-                    <div className='mx-1'>
-                        <a href="https://www.youtube.com/channel/UCyg87GirrddSvia8TqeWepA" target={'blank'}>
+                        <a href="https://dugut.app/data/DuGut-General-Data-Protection-Regulation.pdf" target={'blank'}>
 
-                            <Image src={'/images/common/youtube.svg'} height={15} width={15} />
+                            <p className='mr-2 cursor-pointer'>GDPR</p>
                         </a>
-                    </div>
-                    <div className='mx-1'>
-                        <a href="https://pt.linkedin.com/company/dugutapp" target={'blank'}>
 
-                            <Image src={'/images/common/linkedin.svg'} height={15} width={15} />
+                        <a href="https://dugut.app/terms/DuGut-Terms-of-Use.pdf" target={'blank'}>
+
+                            <p className='mr-2 cursor-pointer'>Terms</p>
                         </a>
+
                     </div>
+
+                    <div className=' flex justify-center mb-3  md:ml-8 md:w-3/4 ' style={{ fontSize: '9px' }}>
+                        <p className='px-1'>DuGut <sup>TM</sup> LDA, 2022, All rights reserved </p>
+                        <Image src={'/images/common/c-circle.svg'} height={10} width={10} />
+                    </div>
+
                 </div>
-
-                <div className=' flex mt-2 mb-1 font-light justify-center text-xs md:flex md:justify-center md:w-3/4  md:ml-8'>
-
-                    <a href="https://dugut.app/privacy/DuGut-Privacy-Policy.pdf" target={'blank'}>
-                        <p className='mr-2 cursor-pointer'>Privacy</p>
-                    </a>
-
-                    <a href="https://dugut.app/data/DuGut-General-Data-Protection-Regulation.pdf" target={'blank'}>
-
-                        <p className='mr-2 cursor-pointer'>GDPR</p>
-                    </a>
-
-                    <a href="https://dugut.app/terms/DuGut-Terms-of-Use.pdf" target={'blank'}>
-
-                        <p className='mr-2 cursor-pointer'>Terms</p>
-                    </a>
-
-                </div>
-
-                <div className=' flex justify-center mb-3  md:ml-8 md:w-3/4 ' style={{ fontSize: '9px' }}>
-                    <p className='px-1'>DuGut <sup>TM</sup> LDA, 2022, All rights reserved </p>
-                    <Image src={'/images/common/c-circle.svg'} height={10} width={10} />
-                </div>
-
             </div>
-
         </div>
     )
 }
